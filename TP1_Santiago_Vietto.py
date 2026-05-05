63eafbdf8a6c6be2bf820e671c40a3d382981d22
santiago-vietto <santiagovietto5@hotmail.com>
2026-05-05 12:13:57 -0300

    Termino tp 1

--- TP1_Santiago_Vietto.py
+++ TP1_Santiago_Vietto.py
@@ -3,7 +3,7 @@ import requests
 
 ##################################################################################
 
-# Paso 1: Conexion con servidor y solicitud de desafio
+# Paso 1: Conexion con servidor, solicitud de desafio y decodificacion Base64.
 
 email = "santiagovietto5@gmail.com"
 known_plaintext = email.encode("ascii")
@@ -15,14 +15,14 @@ response = requests.get(url_challenge)
     
 challenge = response.content
 
-print("\nRE:")
+print("\nRespuesta servidor:")
 print(challenge.decode('ascii'))
 
 ciphertext = base64.b64decode(challenge)
 
-##################################################################################
+#####################################################################################################
 
-# Paso 2: Reducir el Cirfado
+# Paso 2: Eliminacion de clave auxiliar interna y reduccion del texto cirfado.
 
 reduced_ciphertext = b""
 
@@ -34,32 +34,35 @@ for i in range(0, len(ciphertext), 2):
 
     reduced_ciphertext += bytes([resultado])
 
-
 print("\nCantidad de bytes del texto cifrado original:", len(ciphertext))
 print("Cantidad de bytes del texto cifrado reducido:", len(reduced_ciphertext))
 
-##################################################################################
+#####################################################################################################
+
+# Paso 3: Obtencion de los posibles bytes validos de la clave reducida.
 
 hex_digits = b"0123456789abcdef"
-
 valid_key_bytes = set()
 
 for a in hex_digits:
     for b in hex_digits:
         valid_key_bytes.add(a ^ b)
         
-print(valid_key_bytes)
+print("\nValores de bytes validos: ", valid_key_bytes)
 
+#####################################################################################################
 
-##################################################################################
+# Paso 4: Obtencion de la clave reducida y decifrado del texto claro.
 
 key_len = 16
 mensaje = None
 
 for offset in range(0, len(reduced_ciphertext) - len(known_plaintext)):
+    
     posible_clave = {}
 
     for i in range(len(known_plaintext)):
+        
         posicion_clave = (offset + i) % key_len
         valor_clave = reduced_ciphertext[offset + i] ^ known_plaintext[i]
 
@@ -72,6 +75,7 @@ for offset in range(0, len(reduced_ciphertext) - len(known_plaintext)):
         posible_clave[posicion_clave] = valor_clave
 
     if len(posible_clave) == key_len:
+        
         key = bytearray(b"\x00" * key_len)
 
         for posicion, valor in posible_clave.items():
@@ -86,23 +90,22 @@ for offset in range(0, len(reduced_ciphertext) - len(known_plaintext)):
 
         mensaje = plaintext.decode("ascii", errors="replace")
 
-        print("\nOFFSET ENCONTRADO:", offset)
-        print("CLAVE ENCONTRADA:")
-        print(key)
+        print("\nOffset encontrado:", offset)
+        print("Clave encontrada:", key)
 
-        print("\nMENSAJE DESCIFRADO:")
+        print("\nMensaje descifrado:")
         print(mensaje)
 
         break
 
 
 if mensaje is None:
-    print("No se pudo descifrar el mensaje.")
+    print("\nNo se pudo descifrar el mensaje.")
     exit()
 
+#####################################################################################################
 
-##################################################################################
-
+# Paso 5: Envio de la respuesta al servidor.
 
 answer_url = f"{server}/md5crypt/{email}/answer"
 
@@ -113,10 +116,8 @@ response_answer = requests.post(
     }
 )
 
-print("\nRESPUESTA DEL SERVIDOR:")
+print("\nRespuesta del servidor:")
 print("Código:", response_answer.status_code)
 print(response_answer.text)
-
-
 
 
